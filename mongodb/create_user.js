521a52fef0f776f5bb616239d9a214c4f0744db2
白隼 <yonghong.yyh@alibaba-inc.com>
2015-12-21 22:47:43 +0800

    update config

--- mongodb/create_user.js
+++ mongodb/create_user.js
@@ -5,3 +5,31 @@ db.createUser(
     roles: [ { role: "root", db: "admin" } ]
   }
 );
+db.createUser(
+  {
+    user: "spider",
+    pwd: "pntmdcg",
+    roles: [ 
+        {
+            role: "readWrite",
+            db: "test"
+        },
+        {
+            role: "userAdminAnyDatabase",
+            db: "admin"
+        },
+        {
+            role: "readWrite",
+            db: "pyspider_taskdb"
+        },
+        {
+            role: "readWrite",
+            db: "pyspider_projectdb"
+        },
+        {
+            role: "readWrite",
+            db: "pyspider_resultdb"
+        }
+    ]
+  }
+);
