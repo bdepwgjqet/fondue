950a0d04a608fc2ddc20179ceb0027a0e3e1089d
bdepwgjqet <bdepwgjqet@gmail.com>
2016-04-21 07:50:22 +0800

    sync

--- www/luffa/luffa/settings.py
+++ www/luffa/luffa/settings.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import os.path
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -29,7 +29,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 
-    '/home/bdep__/code/acmt/www/luffa/luffa/templates',
+    BASE_DIR + '/luffa/templates',
 )
 
 ALLOWED_HOSTS = []
--- www/script/autostart.sh
+++ www/script/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-HOME=/home/bdep__/code/acmt/www/
+HOME=/home/bdep__/code/fondue/www/
 
 uwsgi --master --die-on-term --emperor $HOME/conf/uwsgi.ini
