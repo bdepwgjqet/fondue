ed57ed372a9d895b4c766c7d79275afe0008d247
bdepwgjqet <bdepwgjqet@gmail.com>
2016-04-21 19:53:34 +0800

    sync

--- www/luffa/luffa/views/index.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render_to_response
-
-__author__ = 'yonghong.yyh'
-
-def index(request):
-    return render_to_response('time/current_datetime.html', locals())
--- www/script/autostart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-HOME=/home/bdep__/code/fondue/www/
-
-uwsgi --master --die-on-term --emperor $HOME/conf/uwsgi.ini
