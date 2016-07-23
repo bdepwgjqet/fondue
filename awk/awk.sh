f06e2c148385cd2c09108798575f50567e426684
bdepwgjqet <bdepwgjqet@gmail.com>
2016-07-23 09:31:28 +0800

    update awk

--- awk/awk.sh
+++ awk/awk.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#basic
 awk -F "" '{print $1,$3}' data
 awk '$1==0 && $3=="LISTEN" {print $0}' data
 awk '$1==0 && $3=="LISTEN"' data
+
+# regex
+
+# end with .png
+awk '$1 ~ /.png$/' data
